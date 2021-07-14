e441a7e6f4cffdd281d3eb8a6fc05a17ba983337
ranjthls <86034304+ranjthls@users.noreply.github.com>
2021-07-14 12:38:25 -0700

    newvue

---
+++ birthdayremainder/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+<HelloWorld name="testing" phone='23432'/>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld"
+export default {
+  name:"App",
+  components : {
+    HelloWorld
+  },
+  data() {
+    return {
+      name: "test",
+      phone:"12345"
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ birthdayremainder/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>Hello {{name}}, {{phone}}, {{gender}}, {{value}}</h1>
+        <button @click="incrementcount">click</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"HelloWorld",
+    props:{
+        name:String,
+        phone:String
+    },
+    data() {
+    return {
+      gender: "Male",
+      value: 0
+    }
+  },
+  methods: {
+      incrementcount(){
+          this.value = this.value + 1
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
